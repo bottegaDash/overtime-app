95c895da80b018c019edf64fb0b0edb42377b455
David Ash <dash@Davids-MacBook-Pro.local>
2018-06-10 11:54:27 -0400

    Added the ablity to connect posts to users

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
+    before do
+      user =  User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+      login_as(user, :scope => :user)
+    end
     it 'can be reached successfully' do
       visit posts_path
       expect(page.status_code).to eq(200)
     end
+
     it 'it has a title of Posts' do
       visit posts_path
       expect(page).to have_content(/Posts/)
@@ -14,6 +19,8 @@ describe 'navigate' do
 
   describe 'creation' do
     before do
+      user =  User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
@@ -23,10 +30,17 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale'
-
       click_on 'Save'
 
       expect(page).to have_content("Some rationale")
+    end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
