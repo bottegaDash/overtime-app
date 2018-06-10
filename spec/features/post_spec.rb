b34d3b311f92e54597594afd3445035178a0d7f6
David Ash <dash@Davids-MacBook-Pro.local>
2018-06-10 18:43:08 -0400

    Implemented index view with posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,26 +1,33 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    @user =  User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(@user, :scope => :user)
+  end
   describe 'index' do
     before do
-      user =  User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      login_as(user, :scope => :user)
+      visit posts_path
     end
     it 'can be reached successfully' do
-      visit posts_path
       expect(page.status_code).to eq(200)
     end
 
     it 'it has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+      expect(post1).to be_valid
+      expect(post2).to be_valid
+      expect(page).to have_content(/Post1|Post2|Post3/)
     end
   end
 
   describe 'creation' do
     before do
-      user =  User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
