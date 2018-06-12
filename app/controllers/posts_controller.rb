42913697f499593e20692beee41ac6dcf69bb5ed
David Ash <dash@Davids-MacBook-Pro.local>
2018-06-12 06:33:42 -0400

    Integrated growl notifications for active record validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
 
     if @post.save
-      redirect_to @post, notice: 'Your post was edited successfully'
+      redirect_to @post, notice: 'Your post was created successfully'
     else
       render :new
     end
